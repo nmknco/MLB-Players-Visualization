fe62d7820a72ebcdd95f809f3c296017672e603d
Lei <nmknco@gmail.com>
2017-12-07 21:58:56 -0500

    Update server file to reflect directory changes

--- server/nodeApp.js
+++ server/nodeApp.js
@@ -3,13 +3,13 @@ var path = require('path');
 var fs = require('fs');
 
 var app = express();
-app.use('/static', express.static(path.join(__dirname, '../static')));
-app.use('/data', express.static(path.join(__dirname, '../data')));
+app.use('/static', express.static(path.join(__dirname, '../0x7f/static')));
+app.use('/data', express.static(path.join(__dirname, '../0x7f/data')));
 
 var port = 7450;
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'v3.html'));
+    res.sendFile(path.join(__dirname, '../0x7f/v3.html'));
 });
 
 app.listen(port, function() {
